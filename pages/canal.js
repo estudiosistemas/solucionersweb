76a7ffcc7e19a7061f4e9bff1853c90f2c36a12a
mCosta7172 <estudiosistemas@gmail.com>
2021-06-07 10:31:28 -0300

    Se agrega seccion canal

--- pages/canal.js
+++ pages/canal.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Image from "next/image";
 import { Container } from "@material-ui/core";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home() {
   const classes = useStyles();
-
+  
   return (
     <>
       <div>
@@ -31,7 +40,142 @@ export default function Home() {
         />
       </div>
       <Container>
-        <div className={classes.root}></div>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Canal"
+                    height="240"
+                    image="https://i.ytimg.com/vi/R_qJks4oKL4/mqdefault.jpg"
+                    title="Todo el contenido del canal Soluciones para todos"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Canal general</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Todos los videos del canal
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link href="https://www.youtube.com/channel/UCjZ8pafpiwPc9cS0vk-OPMg"  target="_blank" rel="noopener">Ver más</Link>
+                </CardActions>
+              </Card>       
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Criptomonedas desde cero"
+                    height="240"
+                    image="https://i.ytimg.com/vi/VIGOexoc2Zg/mqdefault.jpg"
+                    title="Criptomonedas desde cero"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Criptomonedas desde cero</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Un playlist exclusivo para los que recién se inician con las criptomonedas
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link href="https://www.youtube.com/watch?v=VIGOexoc2Zg&list=PL_GbFlpvht1eQCoFMQtyAh1wm6HAZMAVu"  target="_blank" rel="noopener">Ver más</Link>
+                </CardActions>
+              </Card> 
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Inversiones en criptomonedas"
+                    height="240"
+                    image="https://i.ytimg.com/vi/RPrmyXcwsW0/mqdefault.jpg"
+                    title="Inversiones en criptomonedas"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Inversiones en criptomonedas</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Playlist con las distintas inversiones que se pueden realizar con las criptomonedas
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link href="https://www.youtube.com/watch?v=RPrmyXcwsW0&list=PL_GbFlpvht1dCT_n7F6hlAYSo3ztjY71n"  target="_blank" rel="noopener">Ver más</Link>
+                </CardActions>
+              </Card>           
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Todo sobre farming"
+                    height="240"
+                    image="https://i.ytimg.com/vi/yEsDr6GCwI8/mqdefault.jpg"
+                    title="Todo sobre farming"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Todo sobre farming</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Un playlist completo tanto para el que recién se inicia en el farming de criptomonedas como el que busca una estrategia para maximizar sus ganancias
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link href="https://www.youtube.com/watch?v=yEsDr6GCwI8&list=PL_GbFlpvht1ctqPoMMWOjmNT_bdTX9YEG"  target="_blank" rel="noopener">Ver más</Link>
+                </CardActions>
+              </Card>           
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Como hacer trading"
+                    height="240"
+                    image="https://i.ytimg.com/vi/2MEzZkHk7oc/mqdefault.jpg"
+                    title="Como hacer trading"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Como hacer trading</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Aprenda sobre trading de criptomonedas, cuando ingresar a una alerta, take profit, stop loss, análisis de indicadores y mucho más
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link href="https://www.youtube.com/watch?v=2MEzZkHk7oc&list=PL_GbFlpvht1ffjxy4Fy-F2MOZwVBv9vzX"  target="_blank" rel="noopener">Ver más</Link>
+                </CardActions>
+              </Card>    
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Inversiones inmobiliarias con criptomonedas"
+                    height="240"
+                    image="https://i.ytimg.com/vi/j1hz_efIsNQ/mqdefault.jpg"
+                    title="Inversiones inmobiliarias con criptomonedas"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">Inversiones inmobiliarias con criptomonedas</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Información completa sobre las inversiones inmobiliarias con criptomonedas, tips útiles a la hora de comprar y vender un inmueble. Tenga ya una renta fija y despreocupese de las variaciones del mercado
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Link href="https://www.youtube.com/watch?v=j1hz_efIsNQ&list=PL_GbFlpvht1cxhN7inlXJSPbVytVVM0Gc"  target="_blank" rel="noopener">Ver más</Link>
+                </CardActions>
+              </Card> 
+            </Grid>
+          </Grid>
+        </div>
       </Container>
     </>
   );
--- pages/recursos.js
+++ pages/recursos.js
@@ -44,49 +44,49 @@ export default function Home() {
         <div className={classes.root}>
           <h2>Exchanges</h2>
             <List>
-              <ListItemLink href="https://accounts.binance.com/en/register?ref=HURDG2N3">
+              <ListItemLink href="https://accounts.binance.com/en/register?ref=HURDG2N3" target="_blank" rel="noopener">
                 <ListItemText primary="Binance" secondary="20% de descuento en las comisiones" />
               </ListItemLink>
-              <ListItemLink href="https://www.bybit.com/en-US/register/?affiliate_id=12483&language=en-US&group_id=0&group_type=1">
+              <ListItemLink href="https://www.bybit.com/en-US/register/?affiliate_id=12483&language=en-US&group_id=0&group_type=1" target="_blank" rel="noopener">
                 <ListItemText primary="Bybit" secondary="Solo para traders profesionales. Futuros perpetuos USD Bitcoin" />
               </ListItemLink>
-              <ListItemLink href="https://referral.crypto.com/signup?_branch_match_id=900747120523846734">
+              <ListItemLink href="https://referral.crypto.com/signup?_branch_match_id=900747120523846734" target="_blank" rel="noopener">
                 <ListItemText primary="Crypto.com" secondary="Compra y venta de criptomonedas, billetera segura y también da préstamos y podes prestar las tuyas por un interés mensual" />
               </ListItemLink>
-              <ListItemLink href="https://letsbit.io">
-                <ListItemText primary="Let'sBIT " secondary="Compra y venta de criptomonedas en pesos y en dólares (retirar pesos o dólares" />
+              <ListItemLink href="https://letsbit.io" target="_blank" rel="noopener">
+                <ListItemText primary="Let'sBIT " secondary="Compra y venta de criptomonedas en pesos y en dólares (retirar pesos o dólares)" />
               </ListItemLink>
             </List>
           <h2>Canales de la comunidad en Telegram</h2>
             <List>
-              <ListItemLink href="https://t.me/joinchat/6ygYycqVsTFkZjMx">
+              <ListItemLink href="https://t.me/joinchat/6ygYycqVsTFkZjMx" target="_blank" rel="noopener">
                 <ListItemText primary="Noticias" secondary="" />
               </ListItemLink>
-              <ListItemLink href="https://t.me/joinchat/UzTvkA1xFHPhRG-L">
+              <ListItemLink href="https://t.me/joinchat/UzTvkA1xFHPhRG-L" target="_blank" rel="noopener">
                 <ListItemText primary="Principiantes en criptomonedas" secondary="" />
               </ListItemLink>
-              <ListItemLink href="https://t.me/tradinggratissoluciones">
+              <ListItemLink href="https://t.me/tradinggratissoluciones" target="_blank" rel="noopener">
                 <ListItemText primary="Alertas de trading (GRATIS)" secondary="" />
               </ListItemLink>
-              <ListItemLink href="https://t.me/SolucionersInversionInmobiliaria">
+              <ListItemLink href="https://t.me/SolucionersInversionInmobiliaria" target="_blank" rel="noopener">
                 <ListItemText primary="Inversiones inmobiliarias" secondary="Renta fija" />
               </ListItemLink>
-              <ListItemLink href="https://t.me/joinchat/ygEfCbCv40cxMTAx">
+              <ListItemLink href="https://t.me/joinchat/ygEfCbCv40cxMTAx" target="_blank" rel="noopener">
                 <ListItemText primary="Farming" secondary="" />
               </ListItemLink>
-              <ListItemLink href="https://t.me/joinchat/ILG9Bk7u7p41MDYx">
+              <ListItemLink href="https://t.me/joinchat/ILG9Bk7u7p41MDYx" target="_blank" rel="noopener">
                 <ListItemText primary="Non fungible tokens (NFT)" secondary="" />
               </ListItemLink>
-              <ListItemLink href="https://t.me/joinchat/SlH9YUusP7dgypKz">
+              <ListItemLink href="https://t.me/joinchat/SlH9YUusP7dgypKz" target="_blank" rel="noopener">
                 <ListItemText primary="Crypto.com (CRO)" secondary="" />
               </ListItemLink>
             </List>
           <h2>Canales de la comunidad en YouTube</h2>
             <List>
-              <ListItemLink href="https://www.youtube.com/channel/UCjZ8pafpiwPc9cS0vk-OPMg">
+              <ListItemLink href="https://www.youtube.com/channel/UCjZ8pafpiwPc9cS0vk-OPMg" target="_blank" rel="noopener">
                 <ListItemText primary="Canal principal de Soluciones para todos" secondary="" />
               </ListItemLink>
-              <ListItemLink href="https://www.youtube.com/channel/UCBFMkwCqx-yftUNd35dLStA">
+              <ListItemLink href="https://www.youtube.com/channel/UCBFMkwCqx-yftUNd35dLStA" target="_blank" rel="noopener">
                 <ListItemText primary="Canal de noticias" secondary="" />
               </ListItemLink>
             </List>
